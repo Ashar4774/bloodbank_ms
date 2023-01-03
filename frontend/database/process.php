2b4148a1a36f4ef433deacf9ac5a65308f90beb2
ashar4774 <ashar.muhammad74@gmail.com>
2023-01-04 02:33:32 +0500

    changes in frontend

--- frontend/database/process.php
+++ frontend/database/process.php
@@ -4,7 +4,7 @@ session_start();
 //connection with DB
 include "connection.php";
 
-// Request for blood donation
+// Request for blood receiving
 if (isset($_POST['request_submit'])) {
     $receiver_cnic = $_POST['receiver_cnic'];
     $receiver_name = $_POST['receiver_name'];
@@ -17,9 +17,10 @@ if (isset($_POST['request_submit'])) {
 
     $sql = "INSERT INTO `receivers` (`cnic`, `name`, `phone_no`, `abo_type`, `rh_system`, `address`, `bottle_qty`, `status`) VALUES ('$receiver_cnic', '$receiver_name', '$receiver_phone_no', '$receiver_abo_type', '$receiver_rh_system', '$receiver_address', '$receiver_bottle_qty', '$receiver_status' )";
     if ($conn->query($sql) === TRUE) {
-        $_SESSION['receiver_request'] = "Your request has been submitted, We will contact you soon.";
+        $_SESSION['receiver_request'] = "Your request has been submitted, We will contact you soon once admin approve your request.";
         $to = "email@gmail.com";
         $from = "Receiver";
+        $subject = "Need blood";
         $title = "Mail";
         foreach ($_POST as $key => $value) {
             $message_body .= "$key: $value\n";
@@ -28,6 +29,40 @@ if (isset($_POST['request_submit'])) {
         mail($to, $subject, $message_body, $headers);
         header('location:../request.php');
     } else {
-        echo "Error" . $sql . "<br>" . $conn->error;
+        $_SESSION['redceiver_error'] = "There is an error while sending your request, please check your form again";
+        header('location:../request.php');
+        // echo "Error" . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Request for blood donation
+if (isset($_POST['donar_submit'])) {
+    $donar_cnic = $_POST['donar_cnic'];
+    $donar_name = $_POST['donar_name'];
+    $donar_phone_no = $_POST['donar_phone_no'];
+    $donar_abo_type = $_POST['donar_abo_type'];
+    $donar_rh_system = $_POST['donar_rh_system'];
+    $donar_address = $_POST['donar_address'];
+    $donar_bottle_qty = $_POST['donar_bottle_qty'];
+    $donar_status = "pending";
+
+
+    $sql = "INSERT INTO `donars`(`cnic` , `name`, `address`, `phone_no`, `abo_type`, `rh_system`, `status`) VALUES ('" . $donar_cnic . "', '" . $donar_name . "','" . $donar_address . "','" . $donar_phone_no . "','" . $donar_abo_type . "','" . $donar_rh_system . "', '" . $donar_status . "')";
+    if ($conn->query($sql) === TRUE) {
+        $_SESSION['donar_request'] = "Your donation request has been submitted, We will contact you soon once admin approve your request.";
+        $to = "email@gmail.com";
+        $from = "Donar";
+        $subject = "Become a donar";
+        $title = "Mail";
+        foreach ($_POST as $key => $value) {
+            $message_body .= "$key: $value\n";
+        }
+        $headers = "From: $from" . "\r\n" . 'Reply-To: ' . $to . "\r\n";
+        mail($to, $subject, $message_body, $headers);
+        header('location:../request.php');
+    } else {
+        $_SESSION['donar_error'] = "There is an error while sending your request, please check your form again";
+        header('location:../request.php');
+        // echo "Error" . $sql . "<br>" . $conn->error;
     }
 }
--- frontend/request.php
+++ frontend/request.php
@@ -2,7 +2,10 @@
 <html lang="en">
 
 <!-- html header -->
-<?php include('./partials/html_header.php'); ?>
+<?php
+session_start();
+include('./partials/html_header.php');
+ ?>
 <!-- end of html header -->
 
 <body>
@@ -30,6 +33,20 @@
                 </div>
                 <div class="col-lg-6">
                     <div class="bg-light text-center rounded p-5">
+                        <?php 
+                        if (isset($_SESSION['receiver_request'])) {
+                            echo '<div class="alert alert-success mb-0" role="alert">';
+                            echo    $_SESSION['receiver_request'];
+                            echo '</div>';
+                            unset($_SESSION['receiver_request']);
+                        }
+                        if (isset($_SESSION['redceiver_error'])) {
+                            echo '<div class="alert alert-danger mb-0" role="alert">';
+                            echo    $_SESSION['redceiver_error'];
+                            echo '</div>';
+                            unset($_SESSION['redceiver_error']);
+                        }
+                        ?>
                         <h1 class="mb-4">Request for blood</h1>
                         <form method="POST" action="./database/process.php">
                             <div class="row g-3">
@@ -72,6 +89,76 @@
                             </div>
                         </form>
                     </div>
+                </div>
+            </div>
+            <div class="row gx-5 mt-5">
+                <div class="col-lg-6">
+                    <div class="bg-light text-center rounded p-5">
+                        <?php 
+                        if (isset($_SESSION['donar_request'])) {
+                            echo '<div class="alert alert-success mb-0" role="alert">';
+                            echo    $_SESSION['donar_request'];
+                            echo '</div>';
+                            unset($_SESSION['donar_request']);
+                        }
+                        if (isset($_SESSION['donar_error'])) {
+                            echo '<div class="alert alert-danger mb-0" role="alert">';
+                            echo    $_SESSION['donar_error'];
+                            echo '</div>';
+                            unset($_SESSION['donar_error']);
+                        }
+                        ?>
+                        <h1 class="mb-4">Become a donar</h1>
+                        <form method="POST" action="./database/process.php">
+                            <div class="row g-3">
+                                <div class="col-12 col-sm-6">
+                                    <input type="text" class="form-control bg-white border-0" name="donar_cnic" placeholder="CNIC" pattern="[0-9]{5}-[0-9]{7}-{0-9}{1}" style="height: 55px;">
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <input type="text" class="form-control bg-white border-0" name="donar_name" placeholder="Your Name" style="height: 55px;">
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <input type="text" class="form-control bg-white border-0" name="donar_phone_no" placeholder="Phone No" style="height: 55px;">
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <input type="text" class="form-control bg-white border-0" name="donar_bottle_qty" placeholder="Blood bottle qty" style="height: 55px;">
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <select class="form-select bg-white border-0" style="height: 55px;" name="donar_abo_type">
+                                        <option selected>Choose ABO Type</option>
+                                        <option value="A">A</option>
+                                        <option value="B">B</option>
+                                        <option value="AB">AB</option>
+                                        <option value="O">O</option>
+                                    </select>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <select class="form-select bg-white border-0" style="height: 55px;" name="donar_rh_system">
+                                        <option selected>RH System</option>
+                                        <option value="+">+</option>
+                                        <option value="-">-</option>
+                                    </select>
+                                </div>
+                                <div class="col-12 col-sm-12">
+                                    <div class="" id="" >
+                                        <input type="text" class="form-control bg-white border-0" name="donar_address" placeholder="Address" style="height: 55px;">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button class="btn btn-primary w-100 py-3 text-capitalize" type="submit" name="donar_submit">Ask for donation</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <div class="mb-4">
+                        <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5">Appointment</h5>
+                        <h1 class="display-4">Become donar to help needy</h1>
+                    </div>
+                    <p class="mb-5">Eirmod sed tempor lorem ut dolores. Aliquyam sit sadipscing kasd ipsum. Dolor ea et dolore et at sea ea at dolor, justo ipsum duo rebum sea invidunt voluptua. Eos vero eos vero ea et dolore eirmod et. Dolores diam duo invidunt lorem. Elitr ut dolores magna sit. Sea dolore sanctus sed et. Takimata takimata sanctus sed.</p>
+                    <a class="btn btn-primary rounded-pill py-3 px-5 me-3" href="">Find Doctor</a>
+                    <a class="btn btn-outline-primary rounded-pill py-3 px-5" href="">Read More</a>
                 </div>
             </div>
         </div>
